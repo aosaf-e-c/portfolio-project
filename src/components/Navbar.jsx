16351b6b7994bec54131aec40eaf82751231c37e
aosaf-e-c <aosaf@connect.hku.hk>
2025-01-18 22:45:23 +0600

    Add navigation link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,20 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        
-        </div>
+        <Link
+        to="/"
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}>
+        </Link>  
+      </div>
     </nav>
   )
 }
