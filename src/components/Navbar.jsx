c0c41c6fe49a951bacfead5cd5f150a70d82ab6b
aosaf-e-c <aosaf@connect.hku.hk>
2025-01-19 22:17:12 +0600

    Complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 import {styles} from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { menu, close } from '../assets';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -18,6 +19,8 @@ const Navbar = () => {
           setActive("");
           window.scrollTo(0, 0);
         }}>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Aosaf Ershad Chowdhury</p>
         </Link>  
       </div>
     </nav>
