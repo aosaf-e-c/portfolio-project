425647b16815888e1632805db2dcaeb8f5d7e865
aosaf-e-c <aosaf@connect.hku.hk>
2025-02-16 18:22:20 +0800

    add some mesh to thre fibre

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,17 @@
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} 
+        groundColor="black"/>
+      <pointLight intensity={1} />
+    </mesh>
   )
 }
 
